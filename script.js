1d8e650f327e08828b212388006c6f693e86d362
Jakub Siemienowicz <jacobsiemienowicz@gmail.com>
2020-04-06 22:13:42 +0200

    Fixed blocked sounds

--- script.js
+++ script.js
@@ -2,5 +2,6 @@ document.addEventListener('keydown', function(e) {
     console.log(e.keyCode);
     var audio = document.querySelector('audio[data-key="' + e.keyCode +'"]');
     if(!audio) return; //nic nie zwróci
-     audio.play();
+    audio.currentTime = 0; //sprawia że dźwięki się nie blokują
+    audio.play();
 });
